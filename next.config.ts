a6972186c795a8f67d824d31893be2cc0772ef61
nexusbrandsmw <nexusbrandsmw@gmail.com>
2026-07-26 19:56:28 +0200

    Add rewrite rule for WordPress CMS

--- next.config.ts
+++ next.config.ts
@@ -13,3 +13,28 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tiatamw.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/cms/:path*",
+        destination: "http://66.29.148.137/cms/:path*",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
